d120ad639354b411c3f2730e36c1268c86b72418
小熊 <810727305@qq.com>
2021-03-10 12:30:22 +0800

    day01

---
+++ src/components/Child.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-03-10 12:26:36
+ * @LastEditTime: 2021-03-10 12:26:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gulishop-client\src\components\Child.vue
+-->
+<template>
+  
+  <div></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
